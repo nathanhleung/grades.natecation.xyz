1ac2f22aac67d850058087cef11fe4f0a55065e4
nathanhleung <nathanhleung@users.noreply.github.com>
2023-06-27 20:12:51 -0700

    plane work

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Search } from './components/Search'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24 md:max-w-[75%] md:mx-auto">
+    <main className="flex min-h-screen flex-col p-24 md:max-w-[75%] md:mx-auto">
       <div className="flex flex-col text-center">
         <h1 className="text-4xl mb-2"><a href="/">grades.natecation.xyz</a></h1>
         <h2 className="text-lg text-gray-500">Fall 2021, Winter 2022, Spring 2022, and Summer 2022 UCLA Grade Distributions</h2>
