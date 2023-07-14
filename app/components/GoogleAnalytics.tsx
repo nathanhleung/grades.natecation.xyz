813fe83cd30f71796d1e7d1a43048d48661e9872
nathanhleung <nathanhleung@users.noreply.github.com>
2023-07-14 07:07:44 -0800

    add useeffect to track nextjs route changes

--- app/components/GoogleAnalytics.tsx
+++ app/components/GoogleAnalytics.tsx
@@ -1,12 +1,22 @@
 "use client";
 
+import { usePathname } from "next/navigation";
 import Script from "next/script";
+import { useEffect } from "react";
 
 /**
  * Based on
  * https://stackoverflow.com/questions/75749102/how-to-implement-google-analytics-with-nextjs-13
  */
 const GoogleAnalytics = () => {
+    const pathname = usePathname();
+
+    useEffect(() => {
+        (window as any).gtag("config", process.env.NEXT_PUBLIC_GA_TRACKING_ID, {
+            page_path: window.location.href,
+        });
+    }, [pathname]);
+
     return (
         <>
             <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`} />
